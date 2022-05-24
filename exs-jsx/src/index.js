7e1536fb1389cc60fa29e61de70ed9d9e5a9af7e
nazart88 <parlato.developing@gmail.com>
2022-05-24 16:56:47 +0200

    svolgimento esercizio 2

--- exs-jsx/src/index.js
+++ exs-jsx/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Name(props) {
+  return <h1> "Hello", {props.nome} </h1>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const instername = <Name nome="Nazareno" />;
+
+ReactDom.render(instername, document.getElementById("root"));
