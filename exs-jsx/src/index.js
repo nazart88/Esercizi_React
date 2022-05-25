c30ff0182d13ec4883d650e642be22d8c870db9a
nazart88 <parlato.developing@gmail.com>
2022-05-25 10:10:19 +0200

    esercizio 3 svolto

--- exs-jsx/src/index.js
+++ exs-jsx/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Sum(number, number2) {
+  return <h1> "il risultato è" {number + number2} </h1>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const number = 5
+const number2 = 6
+
+
+ReactDom.render(Sum(number, number2), document.getElementById("root"));
