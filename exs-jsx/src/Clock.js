ef5bf73830300d060dd95834130ddf367216c1ed
nazart88 <parlato.developing@gmail.com>
2022-06-16 11:53:33 +0200

    svolgimento esercizio 3

--- exs-jsx/src/Clock.js
+++ exs-jsx/src/Clock.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React from "react"
 
 class Clock extends React.Component {
 
     state = {
-        clock: 0
+        clock: this.props.clock
     }
     constructor(props) {
         super(props)
 
         setInterval(() => {
             this.setState((state) => {
-                return { clock: state.clock + 1 }
+                return { clock: state.clock + this.props.count }
             })
-        }, 1000)
+        }, this.props.interval)
     }
-
 
     render() {
         return <h1>Clock: {this.state.clock}</h1>
     }
+	
 
+}
+
+Clock.defaultProps = {
+    interval: 1000,
+    count: 1,
+    clock: 1,
 }
 
 export default Clock;
